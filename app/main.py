00d3615910400a40635b9ef17d69eda9df3ea782
Vinohradov Volodymyr <eXpertt13@gmail.com>
2025-01-07 16:51:37 +0200

    Implement create_person_list function to manage relationships and link Person instances based on input data.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
+
+    def __repr__(self) -> str:
+        return f"name={self.name!r}, age={self.age!r}"
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list:
+    for person in people:
+        current_person = Person(person["name"], person["age"])
+        spouse_key = "wife" if "wife" in person else "husband"
+        spouse_name = person[spouse_key]
+        if spouse_name in Person.people:
+            spouse = Person.people[spouse_name]
+            if spouse_key == "wife":
+                current_person.wife = spouse
+                spouse.husband = current_person
+            else:
+                current_person.husband = spouse
+                spouse.wife = current_person
+
+    return list(Person.people.values())
